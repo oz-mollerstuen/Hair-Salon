547756d8123e1589adf2c34011e97694505e3ae7
Lucas Mollerstuen <115112679+oz-mollerstuen@users.noreply.github.com>
2022-12-23 08:54:22 -0800

    add clients.cs files

---
+++ HairSalon/Models/Clients.cs
@@ -0,0 +1,13 @@
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    public int ClientId { get; set; }
+    public string Name { get; set; }
+    public string Regular { get; set; }
+    public string Last {get; set; }
+    public int Total {get; set; }
+    public int StylistId { get; set; }
+    public virtual Stylist Stylist { get; set; }
+  }
+}
--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -11,5 +11,10 @@ namespace HairSalon.Models
      public DbSet<Client> Clients { get; set; } 
 
     public HairSalonContext(DbContextOptions options) : base(options) { } 
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
   }
 }
