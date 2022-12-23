df46358d67a2e55ad5fe2adb2c14c2d7cce3f905
Lucas Mollerstuen <115112679+oz-mollerstuen@users.noreply.github.com>
2022-12-23 13:36:35 -0800

    add appsettings.json file

--- HairSalon/Program.cs
+++ HairSalon/Program.cs
@@ -1,40 +1,20 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
-using HairSalon.Models; 
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-namespace HairSalon 
+namespace HairSalon
 {
-  class Program
+  public class Program
   {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
 
-      builder.Services.AddControllersWithViews();
-
-      // Used with ProjectNameContext.cs in Models
-      builder.Services.AddDbContext<HairSalonContext>( 
-                        dbContextOptions => dbContextOptions
-                          .UseMySql(
-                            builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
-                          )
-                        )
-                      );
-
-      WebApplication app = builder.Build();
-
-      // app.UseDeveloperExceptionPage();
-      app.UseHttpsRedirection();
-      app.UseStaticFiles();
-      app.UseRouting();
-
-      app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}"
-      );
-
-      app.Run();
+      host.Run();
     }
   }
 }
