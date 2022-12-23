00daa9049bf6b21d7b57b9d24b8d217ea6ca002b
Lucas Mollerstuen <115112679+oz-mollerstuen@users.noreply.github.com>
2022-12-23 08:43:25 -0800

    add initial files

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -7,7 +7,8 @@ namespace HairSalon.Models
 {
   public class HairSalonContext : DbContext 
   {
-    public DbSet<ClassName> ClassNames { get; set; }  // CHANGE CLASS NAME!!!
+    public DbSet<Stylist> Stylists { get; set; }
+     public DbSet<Client> Clients { get; set; } 
 
     public HairSalonContext(DbContextOptions options) : base(options) { } 
   }
