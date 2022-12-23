4964f679b611db5a39a061c1e3f101d7176ba747
Lucas Mollerstuen <115112679+oz-mollerstuen@users.noreply.github.com>
2022-12-23 09:28:02 -0800

    Add initial code for controllers

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{}
